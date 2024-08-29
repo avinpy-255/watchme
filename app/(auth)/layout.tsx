ca98c367be17dd0a60790479d5afbfa37bbb1e3a
Avinash <avinashchowdhury81@gmail.com>
2024-08-29 17:39:07 +0530

    updated signin page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,22 +5,19 @@ import {ReactNode} from 'react';
 
 export default function AuthLayout({children}: {children: ReactNode}) {
     return (
-       <div  className="h-screen w-screen bg-gradient-to-l from-gray-200 via-fuchsia-200 to-stone-100 ">
-         <div className='relative flex h-screen w-screen flex-col 
-          md:items-center md:justify-center md:bg-transparent'>
-            <div className='mt-24 rounded   py-0 px-0 '>
-              <Image
+<div className="h-screen w-screen bg-gradient-to-l from-gray-200 via-fuchsia-200 to-stone-100 flex items-center justify-center">
+    <div className="flex flex-col items-center justify-center w-full max-w-md px-4">
+        <div className="flex justify-center items-center mb-8">
+            <Image
                 src={logo}
-                alt='Logo'
+                alt="Logo"
                 width={100}
                 height={100}
-                className='items-center'
-              />
-            </div>
-           
-            
-            {children}
+                className="block"
+            />
         </div>
-       </div>
+        {children}
+    </div>
+</div>
     )
 }
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -14,33 +14,14 @@ export default async function Login() {
         return redirect('/home')
     }
     return (
-        <div className="mt-20 rounded py-10 px-6 md:mt-0 md:max-w-sm md:px-14 ">
-        <form method="post " action="/api/auth/signin">
-           <h1 className="text-3xl text-center font-lora">Login</h1>
-           <div className="space-y-4 mt-5">
-               <Input
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                  className=" placeholder:text-xs placeholder:text-grey-400 w-full inline-block"                   
-               />
-                 <Input
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                  className=" placeholder:text-xs placeholder:text-grey-400 w-full inline-block"                   
-               />
-               <Button type="submit" className="w-full bg-indigo-400 hover:bg-indigo-700">login</Button>
-           </div>
-        </form>
-        <div className="mt-1">
-            <p className="text-xs text-center">Dont have account!  <Link href="/sign-up" className="text-indigo-400 hover:text-indigo-700"> Create one</Link></p>
-        </div>
-
-        <div className="flex w-full justify-center items-center gap-x-3 mt-6 ">
-           <GithubSignIn/>
-           <GoogleSignInButton/>
+     <div className="flex flex-col items-center justify-center ">
+      <div className="  py-10 px-6 w-full max-w-sm">
+        <h1 className="text-xl font-semibold text-gray-700 text-center">Continue with:</h1>
+        <div className="flex w-full justify-center items-center gap-x-3 mt-6">
+            <GithubSignIn />
+            <GoogleSignInButton />
         </div>
     </div>
+</div>
     )
 }
