3bf3dfb5f96dbd9e49590aab2882f0ecdffd289d
Avinash <avinashchowdhury81@gmail.com>
2024-08-30 12:23:08 +0530

    vercel error fix 1

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -18,7 +18,7 @@ export default async function AdminPage() {
     <div className="w-full max-w-4xl">
       <div className="min-h-[500px]  flex flex-col justify-center border border-dashed rounded-lg space-y-4">
         <div className="p-10">
-          <ThreeDPhotoCarousel />
+          
         </div>
       </div>
     </div>
