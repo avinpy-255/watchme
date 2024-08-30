cb9f1f5a69ed10ac55a7bc2ada54cc0a17122639
Avinash <avinashchowdhury81@gmail.com>
2024-08-30 23:25:14 +0530

    ui changed 2

--- app/components/MovieVideo.tsx
+++ app/components/MovieVideo.tsx
@@ -31,7 +31,7 @@ export default async function MovieVideo() {
              muted
              loop
              src={data?.videoSource}
-             className="w-full absolute top-0 left-0 h-[60vh] object-cover -z-10 brightness-[70%]"
+             className="w-full absolute top-0 left-0 h-[60vh] object-cover -z-10 brightness-[70%] rounded-2xl shadow-xl"
             ></video>
 
             <div className="absolute w-[90%] lg:w-[40%] mx-auto">
